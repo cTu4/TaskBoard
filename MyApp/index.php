5f8a9b2ee036395df939e307c7f3f6bf258f0377
cTu4 <chudik9898@mail.ru>
2020-08-26 00:58:36 +0300

    aaa

--- MyApp/index.php
+++ MyApp/index.php
@@ -5,7 +5,7 @@
     <script type="text/javascript" src="../extjs/ext-all-debug.js"></script>
     <link rel="stylesheet" href="../css/theme-triton-all.css">
     <link rel="stylesheet" href="../css/style.css">
-    <script type="text/javascript" src="app.js"></script>
+    <script type="text/javascript" src="MyApp.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"
             integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
             crossorigin="anonymous"></script>
